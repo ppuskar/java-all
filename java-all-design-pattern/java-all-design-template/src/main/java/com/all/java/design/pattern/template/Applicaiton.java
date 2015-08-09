a63102678f4d5a87277f327f7e379cc331553ffd
praveen_puskar <puskar.praveen@gmail.com>
2015-08-09 12:25:55 +0530

    fixed compilation error

--- java-all-design-pattern/java-all-design-template/src/main/java/com/all/java/design/pattern/template/Applicaiton.java
+++ java-all-design-pattern/java-all-design-template/src/main/java/com/all/java/design/pattern/template/Applicaiton.java
@@ -1,11 +1,11 @@
 package com.all.java.design.pattern.template;
 
-import com.all.java.design.pattern.template.service.TestSomethingService;
+import com.all.java.design.pattern.template.service.SampleScheduledTaskService;
 
 public class Applicaiton {
 
 	public static void main(String[] args) {
-		AbstractScheduledService service = new TestSomethingService();
+		AbstractScheduledService service = new SampleScheduledTaskService();
 		service.submit(5000);
 	}
 
